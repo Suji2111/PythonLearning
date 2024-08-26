05e796039d51e2bc9a1a3283c0d654dc859330a9
Chandra Babu Naidu <chandrads2111@gmail.com>
2024-08-26 14:03:41 +0530

    Lab exercises dated 22/08/2024 has been added

---
+++ src/ex_22082024/Lab045.py
@@ -0,0 +1,12 @@
+#Loops - Repeat a block of code multiple times
+#For Loop & While Loop
+# Print hello world 10 times
+"""
+Syntax: "for variable_name in range (start, stop, step):"
+
+for, range?
+for i in range(1,10):
+   print(i)
+"""
+x = list(range(1,10,1))
+print(x)
