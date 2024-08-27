c0c93c2eb12a77581fad503e09cccc891437598e
Chandra Babu Naidu <chandrads2111@gmail.com>
2024-08-27 16:06:37 +0530

    Lab exercises dated 24/08/2024 has been added

---
+++ src/ex_24082024/Lab061.py
@@ -0,0 +1,40 @@
+# Functions - reusable set of operations or a block of code which can be reused
+
+import math
+
+
+# 1. Built In.py - Created by Python Guys
+# input()
+# print()
+# max()
+# type()
+# id()
+# pow()
+# len()
+# list()
+# math.cos()
+# range()
+# math.factorial()
+# sorted()
+# int()
+# str()
+# #random()
+# float()
+# bool()
+# str.lower()
+# str.upper()
+
+
+# 2. Custom created, User Defined
+# definition
+def greet():
+    print("Hello, there!!")
+
+
+# calling
+greet()
+greet()
+greet()
+
+
+
